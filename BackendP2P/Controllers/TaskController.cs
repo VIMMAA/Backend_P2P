25bb486f90a17b70a9d4347d61b58e9c548fe7fe
G3nKai <kulikovila2005@gmail.com>
2025-06-20 14:59:28 +0700

    Создание ворк материала

--- BackendP2P/Controllers/TaskController.cs
+++ BackendP2P/Controllers/TaskController.cs
@@ -449,6 +449,46 @@ namespace ApiB.Controllers
                 return StatusCode(500, new { Status = "error", Message = "SWAGA" });
             }
         }
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseModel))]
+        [HttpDelete("{taskId}/workMaterial")]
+        public async Task<IActionResult> DeleteWorkTask(Guid taskId)
+        {
+            IActionResult? authResult = AuthenticateService();
+            if (authResult != null) return authResult;
+
+            try
+            {
+                TaskModel? task = await _context.Tasks.FirstOrDefaultAsync(u => u.Id == taskId);
+
+                if (task == null)
+                {
+                    return NotFound("Task not found");
+                }
+
+                MaterialWorkModel materialWork = await _context.MaterialWorks.FirstOrDefaultAsync(u => u.TaskId == taskId);
+
+                if (materialWork == null)
+                {
+                    return NotFound("Material work not found");
+                }
+
+                _context.MaterialWorks.Remove(materialWork);
+
+                task.MaterialWorkId = null;
+                task.MaterialWorkModel = null;
+
+                _context.Tasks.Update(task);
+                await _context.SaveChangesAsync();
+
+                return Ok(new ResponseModel("Work task deleted"));
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"\nERROR\n{ex}");
+
+                return StatusCode(500, new { Status = "error", Message = "SWAGA" });
+            }
+        }
         private IActionResult? AuthenticateService()
         {
             if (!User.Identity.IsAuthenticated)
