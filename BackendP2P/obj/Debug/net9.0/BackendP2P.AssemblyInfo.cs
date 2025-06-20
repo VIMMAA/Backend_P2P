14a1a7a2cfb6d0dbfe5bda3e6e5d19d8fc3f7052
Vyacheslav Yanushchik <viterbig@mail.ru>
2025-06-21 01:36:59 +0700

    fix

--- BackendP2P/obj/Debug/net9.0/BackendP2P.AssemblyInfo.cs
+++ BackendP2P/obj/Debug/net9.0/BackendP2P.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendP2P")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4210f6dc57c90f68c7c650369fbce359babe110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86dd2a460f4e4600cde147a47953a93b94aa243")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendP2P")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendP2P")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
