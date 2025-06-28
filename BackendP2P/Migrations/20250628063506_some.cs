15bc1e7fdf2a061f6f076b00271b4410c66a0641
G3nKai <kulikovila2005@gmail.com>
2025-06-28 13:38:05 +0700

    fix неработающей БД

---
+++ BackendP2P/Migrations/20250628063506_some.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendP2P.Migrations
+{
+    /// <inheritdoc />
+    public partial class some : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "SolutionId",
+                table: "Reports",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_SolutionId",
+                table: "Reports",
+                column: "SolutionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reports_Solutions_SolutionId",
+                table: "Reports",
+                column: "SolutionId",
+                principalTable: "Solutions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reports_Solutions_SolutionId",
+                table: "Reports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reports_SolutionId",
+                table: "Reports");
+
+            migrationBuilder.DropColumn(
+                name: "SolutionId",
+                table: "Reports");
+        }
+    }
+}
